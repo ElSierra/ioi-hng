30114e6a40bd84874eaba988fd1a94fbcb7822da
ElSierra <isaac.ojo@cset.uniosun.edu.ng>
2022-10-28 18:45:59 +0100

    fixed cors

--- server.js
+++ server.js
@@ -5,12 +5,7 @@ const cors = require("cors");
 const app = express();
 const port = 8080;
 
-app.use(
-  cors({
-    origin: ["http://localhost:8080"],
-    methods: "GET,POST,PUT,DELETE,OPTIONS",
-  })
-);
+app.use(cors());
 
 app.use(bodyParser.json());
 
